4801eb596db67d4dbb494afe9b1ff2f28982835e
briansleonel <gonzalezbrianleonel76@gmail.com>
2022-04-13 20:58:15 -0300

    Start Project

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Ingrese número 1: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese número 2: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2) {
+                Console.WriteLine("Es mayor el número 1");
+            } else if (n1 < n2) {
+                Console.WriteLine("Es mayor el número 2");
+            } else {
+                Console.WriteLine("Ambos números son iguales");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
